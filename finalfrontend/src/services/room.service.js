c54235b5bba64fa1e824a45b53c8737f8b8961ce
marionovaro <marionovaro04@gmail.com>
2023-12-09 09:38:27 +0100

    cambios mario

--- finalfrontend/src/services/room.service.js
+++ finalfrontend/src/services/room.service.js
@@ -1,4 +1,3 @@
-import { updateToken } from "../utils/updateToken";
 import { extraConfig } from "./serviceApiGeneral.config";
 
 //! ------------------ CREATE ------------------
@@ -11,43 +10,43 @@ export const createRoom = async (formData) => {
 };
 
 //! ------------------ UPDATE ------------------
-export const updateRoom = async (userId) => {
+export const updateRoom = async (roomId) => {
   const APIGeneral = extraConfig();
 
-  return APIGeneral.patch(`/rooms/${userId}`)
+  return APIGeneral.patch(`/rooms/${roomId}`)
     .then((res) => res)
     .catch((error) => error);
 };
 
 //! ------------------ DELETE ------------------
-export const deleteRoom = async (userId) => {
+export const deleteRoom = async (roomId) => {
   const APIGeneral = extraConfig();
 
-  return APIGeneral.delete(`/rooms/${userId}`)
+  return APIGeneral.delete(`/rooms/${roomId}`)
     .then((res) => res)
     .catch((error) => error);
 };
 
 //! ----------------- GET by ID -----------------
-export const getRoomById = async (userId) => {
+export const getRoomById = async (roomId) => {
   const APIGeneral = extraConfig();
 
-  return APIGeneral.get(`/rooms/${userId}`)
+  return APIGeneral.get(`/rooms/${roomId}`)
     .then((res) => res)
     .catch((error) => error);
 };
 
 //! ---------------- GET by NAME ----------------
-export const getRoomByName = async (userId) => {
+export const getRoomByName = async (roomName) => {
   const APIGeneral = extraConfig();
 
-  return APIGeneral.get(`/rooms/${userId}`)
+  return APIGeneral.get(`/rooms/byName/${roomName}`)
     .then((res) => res)
     .catch((error) => error);
 };
 
 //! ------------------ GET ALL ------------------
-export const getAllRooms = async (formData) => {
+export const getAllRooms = async () => {
   const APIGeneral = extraConfig();
 
   return APIGeneral.get(`/rooms/`)
@@ -56,19 +55,19 @@ export const getAllRooms = async (formData) => {
 };
 
 //! ------------------- SORT -------------------
-export const sortRooms = async (userId) => {
+export const sortRooms = async (method, value) => {
   const APIGeneral = extraConfig();
 
-  return APIGeneral.get(`/rooms/${userId}`)
+  return APIGeneral.get(`/rooms/sort/rooms/${method}/${value}`)
     .then((res) => res)
     .catch((error) => error);
 };
 
 //! ------------------ FILTER ------------------
-export const filterRooms = async (userId) => {
+export const filterRooms = async (filter, min, max) => {
   const APIGeneral = extraConfig();
 
-  return APIGeneral.get(`/rooms/${userId}`)
+  return APIGeneral.get(`/rooms/filter/rooms/${filter}/${min}/${max}`)
     .then((res) => res)
     .catch((error) => error);
 };
