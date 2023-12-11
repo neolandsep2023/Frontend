ec640173e8050c49e1ac8978fb006ffaba229cf6
abelgh91 <abelgh91@gmail.com>
2023-12-11 09:50:34 +0100

    import useAuth

--- finalfrontend/src/components/ProtectedRoutes/ProtectedCheckChildren.jsx
+++ finalfrontend/src/components/ProtectedRoutes/ProtectedCheckChildren.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
-// import useAuth 
+import { useAuth } from "../../context/authContext";
+
 
 export const ProtectedCheckChildren = ({children}) => {
     const {allUser, user} = useAuth(); 
