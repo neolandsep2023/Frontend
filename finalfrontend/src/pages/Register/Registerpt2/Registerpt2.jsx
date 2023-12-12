9858295e503a1d79425032967e75ca2803c561b1
lemonlux <sololucia17@gmail.com>
2023-12-12 12:49:55 +0100

    registerpt2

--- finalfrontend/src/pages/Register/Registerpt2/Registerpt2.jsx
+++ finalfrontend/src/pages/Register/Registerpt2/Registerpt2.jsx
@@ -3,19 +3,18 @@ import { useAuth } from "../../../context/authContext";
 import { useEffect, useState } from "react";
 import { updateUser } from "../../../services/user.service";
 import { useErrorRegisterpt2 } from "../../../hooks/useErrorRegisterpt2";
+import { FlexDir, Form, H1Form } from "../../../components/StyleComponents";
 
 
 export const Registerpt2 = () => {
-  //! ---- estados
 
 
   const [res, setRes] = useState({});
   const [send, setSend] = useState(false);
 
-  //! ----- hooks
 
   const { setUser, user, logout } = useAuth();
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit } = useForm();
 
 
   const formSubmit = async (formData) => {
@@ -42,6 +41,40 @@ export const Registerpt2 = () => {
 
 
   return (
-    <div>Registerpt2</div>
+    <>
+    <FlexDir>
+    <Form
+          width={"40vw"}
+          height={"80vh"}
+          onSubmit={handleSubmit(formSubmit)}
+        >
+          <FlexDir direction={"column"}>
+          <H1Form margin={"8px 0 0 0"}>Finish Signing up</H1Form>
+
+
+            <FlexDir>
+              <FlexDir direction={"column"}>COLUMNA 1</FlexDir>
+              <FlexDir direction={"column"}>COLUMNA 2</FlexDir>
+
+            </FlexDir>
+
+
+
+            <div>INPUT DESCRIPTTION</div>
+
+          <div>BOTONES</div>
+          </FlexDir>
+
+
+
+
+        </Form>
+
+
+    </FlexDir>
+    
+    
+    </>
+
   )
 }
