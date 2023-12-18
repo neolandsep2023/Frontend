472414d410cc8d5fabc81eec87fe4b1b57d4272b
Alsamu92 <alexsanzm@email.com>
2023-12-18 13:01:03 +0100

    header

--- finalfrontend/src/components/Header/Header.jsx
+++ finalfrontend/src/components/Header/Header.jsx
@@ -7,63 +7,195 @@
   import { useAuth } from "../../context/authContext";
   import { useEffect } from "react";
 
-  export const Header = () => {
-    const { toggleTheme, theme } = useThemeApp();
-    const { user, logout } = useAuth();
-    const navigate = useNavigate();
-    const isDesktop = window.innerWidth < 4000 ? true : false
-    const isTablet = window.innerWidth < 768 ? true : false
-    const isMobile = window.innerWidth < 576 ? true : false 
+import"./Header.css"
+import { HeaderSmall } from './HeaderSmall';
 
-    // console.log(user);
-    const navigateToLogin = () => {
-      navigate("/login");
-    };
-    const navigateToProfile = () => {
-      (isDesktop || isTablet) && navigate("/profile");
-      isMobile && navigate("/profile")
-    }
-
-    return (
-      <div>
-        <HeaderStyle>
-          <FlexDir>
-            <Link to="/">
-            <img
-              alt="logo"
-              src="https://res.cloudinary.com/daxddugwt/image/upload/v1702550399/Untitled_Artwork_33_nmkluc.png"
-            />
-            </Link>
-            <ButtonHeader
-              
-              onClick={toggleTheme}
-            >{theme == "dark" ? "☀️" : "🌙"}
-              
-            </ButtonHeader>
-
-              <ButtonHeader
-                
-                onClick={user == null ? navigateToLogin : logout}
-              >
-                {user == null ? "LOG IN" : "LOG OUT"}
-              </ButtonHeader>
-
-              <>
-                {user !== null && (
-                  <ButtonHeader
-                   
-                    onClick={navigateToProfile}
-                  >
-                    PROFILE
-                  </ButtonHeader>
-                )}
-              </>
-              {/* //no puede haber un navLink y un boton que te redirija. Si hay un navLink y le pones al navLink el redirect en onCLick
-              //y tienes un boton, no pilla el redirect.  Si pones el onCLick en el styledComponent tampoco te lo pilla
-              // solo te lo coge si quitas el navLink y metes el onClick en el boton. Es importante meterle el navigate
-              //instanciado de useNavigate de react, porque solo con el componente <Navigate/> no funciona */}
-          </FlexDir>
-        </HeaderStyle>
-      </div>
-    );
+export const Header = () => {
+  const isMobile = window.innerWidth < 776 ? true : false 
+  const { toggleTheme, theme } = useThemeApp();
+  const navigate = useNavigate()
+  const { user, logout } = useAuth();
+  const navigateToProfile = () => {
+    navigate("/profile");
   };
+
+
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      window.location.reload()
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  
+  return (<>
+  {!isMobile ?<header className='headerDesktop'>
+    <div className="headerImg">
+    <img src="https://res.cloudinary.com/daxddugwt/image/upload/v1702550399/Untitled_Artwork_33_nmkluc.png" alt="Logo de la clínica"/>
+    </div>
+    <div className="navs">
+    <div className="botonesH">
+        <ButtonHeader onClick={toggleTheme}>
+          {theme == "dark" ? "☀️" : "🌙"}
+        </ButtonHeader>
+        <ButtonHeader onClick={user == null ? navigateToProfile : logout}>
+          {user == null ? "LOG IN" : "Profile"}
+        </ButtonHeader>
+        </div>
+        <nav className="navDos">
+           
+            <ul className='mainNav'>
+            
+          
+              <NavLink to={"/feed"} >
+                Feed
+              </NavLink>
+         
+
+           
+           
+              <NavLink to={"/roomSearch"} >
+                Room Search
+              </NavLink>
+          
+
+        
+              <NavLink to={"/createRoom"} >
+                Create Room
+              </NavLink>
+        
+              <NavLink to={"/createPost"} >
+                Create Post
+              </NavLink>
+       
+
+              <NavLink to={"/messages"}>
+                Messages
+              </NavLink>
+       
+          
+              
+           
+            </ul>
+          
+        </nav>
+        </div>
+    </header>:<HeaderSmall/>
+  }
+    
+    </>
+  )
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // export const Header = () => {
+  //   const { toggleTheme, theme } = useThemeApp();
+  //   const { user, logout } = useAuth();
+  //   const navigate = useNavigate();
+  //   const isDesktop = window.innerWidth < 4000 ? true : false
+  //   const isTablet = window.innerWidth < 768 ? true : false
+  //   const isMobile = window.innerWidth < 576 ? true : false 
+
+  //   // console.log(user);
+  //   const navigateToLogin = () => {
+  //     navigate("/login");
+  //   };
+  //   const navigateToProfile = () => {
+  //     (isDesktop || isTablet) && navigate("/profile");
+  //     isMobile && navigate("/profile")
+  //   }
+
+  //   return (
+  //     <div>
+  //       <HeaderStyle>
+  //         <FlexDir>
+  //           <Link to="/">
+  //           <img
+  //             alt="logo"
+  //             src="https://res.cloudinary.com/daxddugwt/image/upload/v1702550399/Untitled_Artwork_33_nmkluc.png"
+  //           />
+  //           </Link>
+  //           <ButtonHeader
+              
+  //             onClick={toggleTheme}
+  //           >{theme == "dark" ? "☀️" : "🌙"}
+              
+  //           </ButtonHeader>
+
+  //             <ButtonHeader
+                
+  //               onClick={user == null ? navigateToLogin : logout}
+  //             >
+  //               {user == null ? "LOG IN" : "LOG OUT"}
+  //             </ButtonHeader>
+
+  //             <>
+  //               {user !== null && (
+  //                 <ButtonHeader
+                   
+  //                   onClick={navigateToProfile}
+  //                 >
+  //                   PROFILE
+  //                 </ButtonHeader>
+  //               )}
+  //             </>
+  //             {/* //no puede haber un navLink y un boton que te redirija. Si hay un navLink y le pones al navLink el redirect en onCLick
+  //             //y tienes un boton, no pilla el redirect.  Si pones el onCLick en el styledComponent tampoco te lo pilla
+  //             // solo te lo coge si quitas el navLink y metes el onClick en el boton. Es importante meterle el navigate
+  //             //instanciado de useNavigate de react, porque solo con el componente <Navigate/> no funciona */}
+  //         </FlexDir>
+  //       </HeaderStyle>
+  //     </div>
+  //   );
+  // };
---
+++ finalfrontend/src/components/Header/HeaderSmall.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "./HeaderSmall.css";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ButtonHeader } from "../StyleComponents";
+import { useThemeApp } from "../../context/themeContext";
+import { useAuth } from "../../context/authContext";
+
+
+export const HeaderSmall = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const navigate = useNavigate()
+  const { toggleTheme, theme } = useThemeApp();
+  const { user, logout } = useAuth();
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <header className="headerSmall">
+      <div className="smallUno">
+        <button className="hamburguesa" onClick={handleMenu}>
+          <span className="material-symbols-outlined">menu</span>
+        </button>
+
+        <img
+          src="https://res.cloudinary.com/daxddugwt/image/upload/v1702550399/Untitled_Artwork_33_nmkluc.png"
+          alt="Logo "
+        />
+        <div className="botonesHS">
+        <ButtonHeader onClick={toggleTheme}>
+          {theme == "dark" ? "☀️" : "🌙"}
+        </ButtonHeader>
+        <button className="buttonHeader" onClick={user == null ? navigateToLogin : logout}>
+          {user == null ? "LOG IN" : "LOG OUT"}
+        </button>
+        </div>
+
+        
+      </div>
+
+      {showMenu && (
+        <div className="zonaMobile mostrar">
+          <nav className="navMobile">
+            <figure className="figureNav">
+              <span className="material-symbols-outlined">home</span>
+              <NavLink to={"/"} onClick={() => setShowMenu(false)}>
+                Home
+              </NavLink>
+            </figure>
+            {user != null ? (
+              <figure className="figureNav">
+                <span className="material-symbols-outlined">
+                  source_environment
+                </span>
+                <NavLink to={"/profile"} onClick={() => setShowMenu(false)}>
+                  Profile
+                </NavLink>
+              </figure>
+            ) : (
+              <figure className="figureNav">
+                <span className="material-symbols-outlined">badge</span>
+                <NavLink to={"/login"} onClick={() => setShowMenu(false)}>
+                  Login
+                </NavLink>
+              </figure>
+            )}
+
+            <figure className="figureNav">
+              <span className="material-symbols-outlined">edit_note</span>
+              <NavLink to={"/feed"} onClick={() => setShowMenu(false)}>
+                Feed
+              </NavLink>
+            </figure>
+
+            <figure className="figureNav">
+              <span className="material-symbols-outlined">diamond</span>
+              <NavLink to={"/roomSearch"} onClick={() => setShowMenu(false)}>
+                Room Search
+              </NavLink>
+            </figure>
+
+            <figure className="figureNav">
+              <span className="material-symbols-outlined">handshake</span>
+              <NavLink to={"/createRoom"} onClick={() => setShowMenu(false)}>
+                Create Room
+              </NavLink>
+            </figure>
+            <figure className="figureNav">
+              <span className="material-symbols-outlined">reduce_capacity</span>
+              <NavLink to={"/createPost"} onClick={() => setShowMenu(false)}>
+                Create Post
+              </NavLink>
+            </figure>
+
+            <figure className="figureNav">
+              <span className="material-symbols-outlined">person_search</span>
+              <NavLink to={"/messages"} onClick={() => setShowMenu(false)}>
+                Messages
+              </NavLink>
+            </figure>
+          </nav>
+        </div>
+      )}
+    </header>
+  );
+};
