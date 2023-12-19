efed0af4fcff1590eb19284d374f4a92b715c8ae
Natalia-dot <nataliadurantegarcia@gmail.com>
2023-12-19 18:14:03 +0100

    can user post??

--- finalfrontend/src/hooks/useCanUserPost.jsx
+++ finalfrontend/src/hooks/useCanUserPost.jsx
@@ -3,20 +3,27 @@ import Swal from "sweetalert2";
 export const useCanUserPost = (user, navigate) => {
     console.log(user)
   if (
-    user.description == "" ||
-    user.name == "" ||
-    user.lastName == "" ||
-    user.birthYear == "" ||
-    user.interests.length == 0
+    user.description == "" || user.description == null ||
+    user.name == "" || user.name == null ||
+    user.lastName == "" || user.lastName == null ||
+    user.birthYear == "" || user.birthYear == null ||
+    user?.interests?.length == 0 || user.interests == null
   ) {
+    console.log("hola")
     const modifyThis = [];
+    const requiredData = ["description", "name", "lastName", "birthYear", "interests"]
+
     console.log(Object.keys(user))
     for (let i = 0; i < Object.keys(user).length; i++) {
-        if (Object.values(user)[i].length === 0) {
+        if (Object.values(user)[i].length === 0 ) {
+          console.log("length 0")
             modifyThis.push(Object.keys(user)[i])
-            console.log('push')
         }
     }
+          for (let i = 0; i < requiredData.length; i++) {
+            user[requiredData[i]] ? console.log("hola") : modifyThis.push(requiredData[i])
+            
+          }
     let joint = modifyThis.join(", ")
     navigate("/profile");
     return Swal.fire({
